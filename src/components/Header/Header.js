86b4cf4701e248a809abef0c7306960e0cbd7538
Pilip.Shchasny <c-pshchasny@neogov.net>
2021-12-24 18:06:02 +0300

    pshchasny-added-header-and-some-icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import styled from "styled-components";
+import {Avatar} from "@mui/material";
+import {AccessTime} from "@mui/icons-material";
 
 const Header = () => {
   return <HeaderContainer>
-    <h1>test</h1>
+    {/* Header left */}
+    <HeaderLeft>
+      <HeaderAvatar
+      // TODO Add on click
+      />
+      <AccessTime/>
+    </HeaderLeft>
+    {/* Header Search */}
+
+    {/* Header Right */}
   </HeaderContainer>
 };
 
 export default Header;
 
-const HeaderContainer = styled.div`
-  
-`;
+const HeaderContainer = styled.div``;
+const HeaderLeft = styled.div``;
+const HeaderAvatar = styled(Avatar)``;

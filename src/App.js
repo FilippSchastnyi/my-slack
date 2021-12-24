55efe91160f1af4bf6ed14c0d0df3210029e2c95
Pilip.Shchasny <c-pshchasny@neogov.net>
2021-12-24 17:33:18 +0300

    pshchasny-added-styled-component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Header from "./components/Header/Header";
 
 
 function App() {
   return (
     <Router>
-    <>
-      <h1>Welcome to React Router!</h1>
-      <Routes>
-        <Route path="/"/>
-      </Routes>
-    </>
+      <>
+        <Routes>
+          <Route path="/"
+                 element={<Header/>}/>
+        </Routes>
+      </>
     </Router>
   );
 }
